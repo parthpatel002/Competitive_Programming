6f001aa643dc0c12a6e2fce3107472168528b078
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-17 15:54:56 +0530

    Create bitwise_operations.cpp

---
+++ C++_Code_Snippets/bitwise_operations.cpp
@@ -0,0 +1,17 @@
+// XOR (Exclusive OR)
+int a = b ^ c;
+
+// AND
+int a = b & c;
+
+// OR
+int a = b | c;
+
+// NOT
+int a = ~b;
+
+// Left Shift by 2 bits
+int a = b << 2;
+
+// Right Shift by 2 bits
+int a = b >> 2;
