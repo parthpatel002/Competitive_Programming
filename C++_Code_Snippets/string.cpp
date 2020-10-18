bc4c5bb717fb86c9f9de58bc37bd3c18cfebdd15
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-18 13:53:29 +0530

    Update string.cpp

--- C++_Code_Snippets/string.cpp
+++ C++_Code_Snippets/string.cpp
@@ -2,3 +2,9 @@
 if (str.find(str1, 0) != string::npos) 
         cout << "First occurrence is " << str.find(str1, 0) << endl; 
 
+// Substring Function
+string s = "Parth"; 
+// Copy three characters of s1 (starting from position 1) 
+string s1 = s.substr(1, 3); // "art"
+string s2 = s.substr(1); // "arth"
+
