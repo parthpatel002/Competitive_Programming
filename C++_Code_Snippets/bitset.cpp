45cdb80783cd9674e4b020ca698d4e02f2959208
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-18 21:34:25 +0530

    Create bitset.cpp

---
+++ C++_Code_Snippets/bitset.cpp
@@ -0,0 +1,7 @@
+// BitSet is much more efficient than unordered_map, and should be used whenever possible.
+// The size of bitset is fixed at compile time, i.e. it can’t be changed at runtime.
+
+// Initialise a bitset with M bits
+# define M 8
+bitset<M> bset;
+
