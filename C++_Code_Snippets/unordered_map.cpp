4ecff81d16b7f7bfa4ae8698647ae9ffa5a76d7e
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-19 12:19:21 +0530

    Update unordered_map.cpp

--- C++_Code_Snippets/unordered_map.cpp
+++ C++_Code_Snippets/unordered_map.cpp
@@ -13,3 +13,14 @@ else
 for(auto it = umap.begin(); it != umap.end(); it++)
   cout << "Key :" << it->first << "; Val :" << it->second << endl;
   
+// Delete a key
+umap.erase(key);
+
+// Initialise empty unordered_map in a class constructor
+class RandomizedSet{
+private:
+    unordered_map<int, int> umap1;
+    unordered_map<int, int> umap2;  
+public:
+    RandomizedSet() : umap1({}), umap2(unordered_map<int, int>()) {}
+};
