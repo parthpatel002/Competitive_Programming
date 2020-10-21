3a22446781147927c248a1df7e9dac9a307de658
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-21 15:52:35 +0530

    Create stack.cpp

---
+++ C++_Code_Snippets/stack.cpp
@@ -0,0 +1,18 @@
+// Initialise empty stack
+stack<int> st;
+
+// Check if stack is empty or not
+st.empty();
+
+// Returns the size of the stack 
+st.size();
+
+// Returns a reference to the top most element of the stack 
+st.top();
+
+// Adds the element ‘g’ at the top of the stack
+st.push(g);
+
+// Deletes the top most element of the stack
+st.pop();
+
