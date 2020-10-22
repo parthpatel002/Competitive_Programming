51e0ced18df0977e96d9c804be846c541a572b80
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-22 11:43:40 +0530

    Create queue.cpp

---
+++ C++_Code_Snippets/queue.cpp
@@ -0,0 +1,19 @@
+// Initialise Empty Queue
+queue<pair<int, int>> q;
+
+// Check whether the queue is empty 
+q.empty();
+
+// Find size of queue
+q.size();
+
+// Front and back elements of queue
+q.front();
+q.back();
+
+// Add element to end of queue
+q.push(make_pair(v1, v2));
+
+// Delete first element of queue
+q.pop();
+
