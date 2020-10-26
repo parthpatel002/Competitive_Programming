387622156e0796e5da7e857ba43a9f0b9d18caa7
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-26 13:59:28 +0530

    Update list.cpp

--- C++_Code_Snippets/list.cpp
+++ C++_Code_Snippets/list.cpp
@@ -45,9 +45,32 @@ ls.clear();
 // Iterate over elements of the list
 list<int>::iterator it; 
 for(it = ls.begin(); it != ls.end(); it++) 
-	  cout << " " << *it; 
+    cout << " " << *it; 
 
-// 
+// Insert element into list
+list<int>::iterator it = ls.begin(); 
+advance(it, 2); // iterator to point to 3rd position 
+ls.insert(it, 5); // using insert to insert 1 element (i.e. 5) at the 3rd position 
+    
+// Insert elements into list
+list<int>::iterator it = ls.begin(); 
+advance(it, 2); // iterator to point to 3rd position 
+ls.insert(it, 2, 7); // insert 2 occurrences of 7 at the 3th position 
+
+// Delete element from list
+list<int>::iterator it = ls.begin(); 
+advance(it, 2); // iterator to point to 3rd position 
+ls.erase(it); // remove element at 3rd position 
+
+// Delete elements from list
+list<int>::iterator itr1, itr2; 
+itr1 = demoList.begin(); 
+itr2 = demoList.begin(); 
+advance(itr2, 3); // Incrementing itr2 by 3 positions 
+demoList.erase(itr1, itr2); // deleting range of elements from index [0, 3) 
+
+// Remove element with certain value from list
+ls.remove(20); // delete all elements with value 20 
 
 // Reverse the list in-place
 ls.reverse();
