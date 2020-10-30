0568856d0edad5d8da14cdf5e90b254fbac559bb
Parth Patel <parthshaileshpatel98@gmail.com>
2020-10-30 22:35:31 +0530

    Update sorting.cpp

--- C++_Code_Snippets/sorting.cpp
+++ C++_Code_Snippets/sorting.cpp
@@ -2,6 +2,11 @@
 #include <bits/stdc++.h> 
 sort(vec.begin(), vec.end());
 
+// Sorting a vector of vectors [according to first element and resolving ties with subsequent elements].
+// Useful for vector of size-2 vectors (https://github.com/parthpatel002/Competitive_Programming/blob/main/LeetCode/56-Merge-Intervals.cpp) .
+vector<vector<int>> vec;
+sort(vec.begin(), vec.end());
+
 // Sorting a vector [named 'points' below] of some object (below, a vector of 2 elements):
 #include <bits/stdc++.h> 
 struct compare{
