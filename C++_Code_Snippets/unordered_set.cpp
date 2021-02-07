4e0babf49cbbc9d2f9e1c5d7c3e1a48fbd2e4bc7
Parth Patel <parthshaileshpatel98@gmail.com>
2021-02-07 13:37:19 +0530

    Update unordered_set.cpp

--- C++_Code_Snippets/unordered_set.cpp
+++ C++_Code_Snippets/unordered_set.cpp
@@ -4,7 +4,15 @@ unordered_set<int> uset();
 // Insert a val
 uset.insert(val);
 
+// Find value
+if (uset.find(val) == uset.end()) 
+    cout << val << " not found" << endl; 
+else
+    cout << "Found " << val << endl;
+
+// Remove value
+uset.erase(val);
+
 // Compute union of uset1 and uset2
 unordered_set<int> union_set(uset1);
 union_set.insert(uset2.begin(), uset2.end());
-
