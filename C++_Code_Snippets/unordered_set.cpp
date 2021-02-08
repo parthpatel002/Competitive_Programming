7480b3996c8bd4d2dfa3e255ee1f7572d3197cdf
Parth Patel <parthshaileshpatel98@gmail.com>
2021-02-08 13:52:22 +0530

    Update unordered_set.cpp

--- C++_Code_Snippets/unordered_set.cpp
+++ C++_Code_Snippets/unordered_set.cpp
@@ -13,6 +13,11 @@ else
 // Remove value
 uset.erase(val);
 
+// Iterate over elements
+unordered_set<int>::iterator itr; 
+for (itr = uset.begin(); itr != uset.end(); itr++) 
+    cout << (*itr) << endl; 
+
 // Compute union of uset1 and uset2
 unordered_set<int> union_set(uset1);
 union_set.insert(uset2.begin(), uset2.end());
