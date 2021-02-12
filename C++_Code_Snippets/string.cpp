1f52b75b3482f46ac39927585047e98003c309c2
Parth Patel <parthshaileshpatel98@gmail.com>
2021-02-12 11:28:56 +0530

    Update string.cpp

--- C++_Code_Snippets/string.cpp
+++ C++_Code_Snippets/string.cpp
@@ -8,6 +8,19 @@ string s = "Parth";
 string s1 = s.substr(1, 3); // "art"
 string s2 = s.substr(1); // "arth"
 
+// Compare strings
+void relationalOperation(string s1, string s2){
+    if (s1 != s2) {
+        cout << s1 << " is not equal to " << s2 << endl;
+        if (s1 > s2)
+            cout << s1 << " is greater than " << s2 << endl;
+        else
+            cout << s2 << " is greater than " << s1 << endl;
+    }
+    else
+        cout << s1 << " is equal to " << s2 << endl;
+}
+
 // String to Integer
 string s = "123";
 int fid = stoi(s);
